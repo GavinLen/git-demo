80984ef3664dd10e4358cf34a33e43af0ac03d56
gavin_len <gavin_len@163.com>
2019-07-19 03:13:36 +0800

    DEV: 20190819 添加 spring-boot-config 模块

---
+++ spring-boot-config/src/main/java/xyz/ieden/config/ConfigApplication.java
@@ -0,0 +1,12 @@
+package xyz.ieden.config;
+
+/**
+ * @author gavin
+ * @version 1.0
+ * @date 2019/7/19 3:11
+ */
+public class ConfigApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ spring-boot-config/src/test/java/xyz/ieden/config/ConfigApplicationTest.java
@@ -0,0 +1,12 @@
+package xyz.ieden.config;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConfigApplicationTest {
+
+    @Test
+    public void mainTest() {
+    }
+}
